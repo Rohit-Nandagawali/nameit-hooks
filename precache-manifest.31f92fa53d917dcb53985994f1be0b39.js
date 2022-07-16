96a00e7c5cd68ae6ea2ceb6c8a39c45ae957360e
Rohit Sudhir Nandagawali <nandagawalirohit143@gmail.com>
2022-07-16 16:22:40 +0530

    Updates

---
+++ precache-manifest.31f92fa53d917dcb53985994f1be0b39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7da999e02dece0858035",
+    "url": "/nameit-hooks/static/css/main.6ed687ca.chunk.css"
+  },
+  {
+    "revision": "7da999e02dece0858035",
+    "url": "/nameit-hooks/static/js/main.7da999e0.chunk.js"
+  },
+  {
+    "revision": "b946d4f22d331bc54ac6",
+    "url": "/nameit-hooks/static/js/1.b946d4f2.chunk.js"
+  },
+  {
+    "revision": "45eb563d9c8f8e06bc5d",
+    "url": "/nameit-hooks/static/js/runtime~main.45eb563d.js"
+  },
+  {
+    "revision": "ba4d9716f2a028426d01c5a4a909bf94",
+    "url": "/nameit-hooks/index.html"
+  }
+];
